338981b7cb7ca9594a69a7e092652b1b2e28a9bc
Gabriel <gabrielpaes99@gmail.com>
2024-02-20 14:35:20 -0300

    app.route changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { UserGalleryComponent } from './pages/user-gallery/user-gallery.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'store', component: AboutComponent },
+  { path: 'store', component: UserGalleryComponent },
   { path: 'about-us', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
 ];
